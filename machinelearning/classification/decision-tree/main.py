84016f13027b5590eee8eb70043cfba363083a1e
jainayushjain95 <jainayushjain95@gmail.com>
2025-02-07 22:01:39 +0530

    dtc and rfc

--- machinelearning/classification/decision-tree/main.py
+++ machinelearning/classification/decision-tree/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 print('\nStep 2 - Importing the dataset')
 dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, 0:-1].values
+X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 print('\nStep 3 - Splitting the dataset into the Training set and Test set')
@@ -18,9 +18,11 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-print('\nStep 5 - Training the S (also called kernel SVM) model on the Training set')
-from sklearn.svm import SVC
-classifier = SVC(kernel='linear', random_state=0)
+print('\nStep 5 - Training the Decision Tree Model on the Training set')
+from sklearn.tree import DecisionTreeClassifier
+# Initialize the Decision Tree Classifier with 'entropy' as the criterion
+# 'entropy' helps the tree decide the best splits based on information gain
+classifier = DecisionTreeClassifier(criterion='entropy', random_state=0)
 classifier.fit(X_train, y_train)
 
 print('\nStep 6 - Predicting a new result')
@@ -28,7 +30,7 @@ print(classifier.predict(sc.transform([[30, 87000]])))
 
 print('\nStep 7 - Predicting the Test set results')
 y_predict = classifier.predict(X_test)
-print(np.concatenate((y_predict.reshape(len(y_predict),1), y_test.reshape(len(y_test),1)),1))
+print(np.concatenate((y_test.reshape(len(y_test), 1), y_predict.reshape(len(y_test), 1)), 1))
 
 print('\nStep 8 - Making the Confusion Matrix')
 from sklearn.metrics import confusion_matrix, accuracy_score
@@ -37,18 +39,20 @@ print(cm)
 accuracy_score = accuracy_score(y_test, y_predict)
 print(accuracy_score)
 
+step = 0.25
+
 print('\nStep 9 - Visualizing the Training Set Results')
 from matplotlib.colors import ListedColormap
 X_set, y_set = sc.inverse_transform(X_train), y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
-                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = step),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = step))
 plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('SVM-NL (Training set)')
+plt.title('Decision Tree Model (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -57,15 +61,15 @@ plt.show()
 print('\nStep 10 - Visualizing the Test Set Results')
 from matplotlib.colors import ListedColormap
 X_set, y_set = sc.inverse_transform(X_test), y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
-                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = step),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = step))
 plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('SVM-NL (Test set)')
+plt.title('Decision Tree Model (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
